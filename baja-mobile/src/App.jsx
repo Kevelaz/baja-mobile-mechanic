a4e15c6aa9277e0a65c7ae70c8bd5a88d6f63f69
Kevelaz <kevinvelazquez925@gmail.com>
2024-02-29 22:54:42 -0500

    add some more small styling

--- baja-mobile/src/App.jsx
+++ baja-mobile/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import NavigationBar from './components/NavBar/NavBar.jsx'
 import LandingPage from './components/LandingPage/LandingPage.jsx'
 import SchedulingPage from './components/SchedulingPage/SchedulingPage.jsx'
-// import './App.css'
+import './App.css'
 
 function App() {
 
