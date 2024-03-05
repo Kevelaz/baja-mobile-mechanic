fa40e7850f7e73769e396fd6cc70c44408fea28a
Kevelaz <kevinvelazquez925@gmail.com>
2024-03-05 16:58:16 -0500

    add background image'

--- baja-mobile/src/pages/Landing/Landing.jsx
+++ baja-mobile/src/pages/Landing/Landing.jsx
@@ -8,7 +8,7 @@ const Landing = () => {
     <div className="container-fluid h-100 d-flex align-items-center justify-content-center">
       <div className="text-center">
         {/* Text content */}
-        <div id="paragraph" className="text-danger">
+        <div id="paragraph" >
           <h1 id="title">About Us</h1>
           <h5>
             Welcome to Baja Mobile Mechanic, where automotive care comes to you. We're not just a mechanic service; we're your on-the-go solution to automotive maintenance and repair. At Baja Mobile Mechanic, we understand that life doesn't always pause for a trip to the mechanic. That's why we bring our expertise directly to your doorstep, whether you're at home, work, or stuck on the side of the road.
