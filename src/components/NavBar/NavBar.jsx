c98bb56aee7288f0062095bd6e276f52c94982f0
Kevelaz <kevinvelazquez925@gmail.com>
2024-03-08 15:07:46 -0500

    changes file  path for images

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Navbar, Nav} from 'react-bootstrap'
 import { Link } from "react-router-dom";
+import Logo from "../../assets/imgs/logo-5.png"
 import './NavBar.css'
 
 const NavigationBar = () => {
   return (
     <Navbar variant="dark" expand="lg" id="nav-bar-container">
       <Navbar.Brand href="/" className="brand-name">
-        <img src="public/imgs/logo-5.png" alt="BMM logo" className="logo-img" />
+        <img src={Logo} alt="BMM logo" className="logo-img" />
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-Navbar-nav" />
       <Navbar.Collapse id="basic-Navbar-nav" className="justify-content-end nav-pills">
