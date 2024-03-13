d7e413b83132c59ba231f818f75c6f9b52bce855
Kevelaz <kevinvelazquez925@gmail.com>
2024-03-13 11:21:06 -0400

    deleted dead code and added comments

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,16 +8,16 @@ import './NavBar.css'
 const NavigationBar = () => {
   return (
     <Navbar variant="dark" expand="lg" id="nav-bar-container">
-      <Navbar.Brand href="/" className="brand-name">
-        <img src={Logo} alt="BMM logo" className="logo-img" />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-Navbar-nav" />
-      <Navbar.Collapse id="basic-Navbar-nav" className="justify-content-end nav-pills">
-        <Nav className="mr-auto">
-          <Nav.Link to="/" href="/">About Us</Nav.Link>
-          <Link to="/book-appointment" className="book-app">Book an Appointment</Link>
-        </Nav>
-      </Navbar.Collapse>
+        <Navbar.Brand href="/" className="brand-name">
+          <img src={Logo} alt="BMM logo" className="logo-img" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-Navbar-nav" />
+        <Navbar.Collapse id="basic-Navbar-nav" className="justify-content-end nav-pills">
+          <Nav className="mr-auto">
+            <Nav.Link to="/" href="/">About Us</Nav.Link>
+            <Link to="/book-appointment" className="book-app">Book an Appointment</Link>
+          </Nav>
+        </Navbar.Collapse>
     </Navbar>
   )
 }
--- src/components/SchedulingPage/SchedulingPage.jsx
+++ src/components/SchedulingPage/SchedulingPage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Scheduling from "../../pages/Scheduling/Scheduling";
 
-
 const SchedulingPage = () => {
   return (
     <>
